422afb4028c8a85c07939d7a2e928212fd60ff81
Lorenz Hänggi <lolo8304@gmail.com>
2023-06-05 00:51:02 +0200

    test parallel calls - step 3

---
+++ no-11/parallel.sh
@@ -0,0 +1,10 @@
+curl -XGET http://localhost:8080/index.html &
+curl -XGET http://localhost:8080/index.html &
+curl -XGET http://localhost:8080/index.html &
+curl -XGET http://localhost:8080/index.html &
+curl -XGET http://localhost:8080/index.html &
+curl -XGET http://localhost:8080/index.html &
+curl -XGET http://localhost:8080/index.html &
+curl -XGET http://localhost:8080/index.html &
+curl -XGET http://localhost:8080/index.html &
+
