dde8ce76ae4f9f862a12f743135a30240a491fa9
Lorenz Hänggi <lolo8304@gmail.com>
2023-07-16 05:58:32 +0200

    add new test case for get key key

--- no-17/app/src/test/java/ClientTest.java
+++ no-17/app/src/test/java/ClientTest.java
@@ -103,18 +103,50 @@ class ClientTest {
             var value = randomKey("testvalue");
 
             // Act
-            var cmd = new SetCommand(key, value, 31, 300, false);
+            var cmd = new SetCommand(key, value, 31, 300, true);
             Optional<Response> response = client.sendCommand(cmd);
 
             var cmdGet = new GetCommand(key);
+            var responseGet = client.sendCommand(cmdGet);
+            var responseCmd = responseGet.get().cmds.get(0);
+
+            // Assert
+            assertTrue(started);
+            assertEquals(false, response.isPresent());
+            assertEquals(true, responseGet.isPresent());
+            assertEquals(31, responseCmd.flags());
+            assertEquals(value, responseCmd.data.data);
+        } finally {
+            client.close();
+        }
+
+    }
+
+    @Test
+    void getcmd2_simple_expectsValue() throws URISyntaxException, IOException {
+
+        // Arrange
+        var client = new MemcachedClient("localhost:11212");
+        try {
+            var started = client.start();
+            var key = randomKey("asdf");
+            var value = randomKey("testvalue");
+
+            // Act
+            var cmd = new SetCommand(key, value, 31, 300, false);
+            Optional<Response> response = client.sendCommand(cmd);
+
+            var cmdGet = new GetCommand(key, key);
             Response responseGet = client.sendCommand(cmdGet).get();
             var responseCmd = responseGet.cmds.get(0);
+            var responseCmd2 = responseGet.cmds.get(1);
 
             // Assert
             assertTrue(started);
             assertEquals(true, response.isPresent());
             assertEquals(31, responseCmd.flags());
             assertEquals(value, responseCmd.data.data);
+            assertEquals(value, responseCmd2.data.data);
         } finally {
             client.close();
         }
