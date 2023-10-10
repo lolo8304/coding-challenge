14d8b1339e21cf9e8e49974b520e834b040916fb
Lorenz aka Lolo Hänggi <lolo8304@gmail.com>
2023-10-10 08:04:15 +0200

    Update Parser.java

--- no-30/app/src/main/java/lisp/parser/Parser.java
+++ no-30/app/src/main/java/lisp/parser/Parser.java
@@ -3,5 +3,17 @@ public class Parser {
   public Parser(Tokenizer tokenizer) {
     this.tokenizer = tokenizer:
   }
-  
+  public Parser(BufferedReader reader) {
+    this.tokenizer = new Tokenizer(reader);
+  }
+
+  public List<TokenValue> parse() {
+    var token = this.tokenizer.nextToken();
+    var expressions = new ArrayList<TokenValue>();
+    while (token.isPresent()) {
+        expressions.add(token.get());
+        token = this.tokenizer.nextToken();
+    }
+    
+  }
 }
