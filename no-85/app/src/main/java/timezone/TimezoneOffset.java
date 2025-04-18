3734f97d335a3b5b1fb01e029c02af99e337b62a
Lorenz <lorenz.haenggi@bluewin.ch>
2025-04-18 22:40:21 +0200

    impl search by id, city, country and support DST

--- no-85/app/src/main/java/timezone/TimezoneOffset.java
+++ no-85/app/src/main/java/timezone/TimezoneOffset.java
@@ -1,18 +1,16 @@
 package timezone;
 
-public record TimezoneOffset(String offsetMMHH, boolean plus) implements Comparable {
+public record TimezoneOffset(String offsetMMHH, boolean plus) implements Comparable<TimezoneOffset> {
     public TimezoneOffset(String offset) {
         this(offset.substring(1), offset.charAt(0) == '+');
     }
 
     @Override
-    public int compareTo(Object o) {
+    public int compareTo(TimezoneOffset o) {
         if (o instanceof TimezoneOffset(String otherMMYYY, boolean otherPlus)) {
             if (this.plus == otherPlus) return (this.plus ? 1 : -1) * this.offsetMMHH.compareToIgnoreCase(otherMMYYY);
             if (!this.plus) return -1;
             return 1;
-        } else if (o instanceof String s && !s.isBlank()) {
-            return this.compareTo(new TimezoneOffset(s));
         }
         throw new RuntimeException("Cannot compare this object " + o);
     }
