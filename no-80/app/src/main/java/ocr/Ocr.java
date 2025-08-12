69a76dfb425d3e9fdf76f4ed1e4cf787e3ef5d17
Lorenz Hänggi <lolo8304@gmail.com>
2025-08-12 10:28:24 -0400

    inital app for OCR

---
+++ no-80/app/src/main/java/ocr/Ocr.java
@@ -0,0 +1,47 @@
+package ocr;
+
+
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+
+import java.io.IOException;
+import java.util.concurrent.Callable;
+
+@Command(name = "ocr", mixinStandardHelpOptions = true, version = "ocr 1.0", description = "This challenge is to build your own Optical Character Recognition")
+public class Ocr implements Callable<Result> {
+
+    public static int _verbose = 0;
+
+    @Option(names = "-v", description = "verbose model level 1")
+    boolean verbose = false;
+    @Option(names = "-vv", description = "verbose model level 2")
+    boolean verbose2 = false;
+
+    public static void main(String[] args) {
+        var ocr = new Ocr();
+        var cmd = new CommandLine(ocr);
+        var exitCode = cmd.execute(args);
+        Result result = cmd.getExecutionResult();
+        if (result != null && result.toString() != null) {
+            System.exit(exitCode);
+        }
+    }
+
+    public static boolean verbose() {
+        return _verbose >= 1;
+    }
+
+    @SuppressWarnings("unused")
+    public static boolean verbose2() {
+        return _verbose >= 2;
+    }
+
+    @Override
+    public Result call() throws IOException {
+        if (this.verbose) _verbose = 1;
+        if (this.verbose2) _verbose = 2;
+        return new Result();
+    }
+
+}
---
+++ no-80/app/src/main/java/ocr/Result.java
@@ -0,0 +1,5 @@
+package ocr;
+
+public class Result {
+
+}
